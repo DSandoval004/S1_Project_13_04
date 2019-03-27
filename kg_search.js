6bc5fccfb4e9c8cee2098891de24eba2c522f8cd
Diego Sandoval <dsandoval004@west-mec.org>
2019-03-27 15:28:30 -0700

    Update 3/27/19
    Add hover effects and select effects

--- kg_search.js
+++ kg_search.js
@@ -24,20 +24,101 @@
       on the items in the list array
 
 */
+// DVARG:
+var mouseUP = true,
+      WSCells;
 // DDOES:
 window.onload = function () {
       // DDOES:
       document.getElementById('wordSearchTitle').innerHTML = wordSearchTitle;
       document.getElementById('wordTable').innerHTML = drawWordSearch(letterGrid, wordGrid);
       document.getElementById('wordList').innerHTML = showList(wordArray);
+      // DVARA:
+      WSCells = document.querySelectorAll('table#wordSearchTable td')
+      // DLOOP:
+      for (var i = 0; i < WSCells.length; i++) {
+            // DDOES:
+            WSCells[i].style.cursor = "pointer";
+            // DDOES:
+            WSCells[i].addEventListener('mouseenter', function (e) {
+                  // DIFDO:
+                  if (mouseUP) e.target.style.backgroundColor = "lightGray";
+
+            });
+            // DDOES:
+            WSCells[i].addEventListener('mouseleave', function (e) {
+                  // DIFDO:
+                  if (mouseUP) e.target.style.backgroundColor = "white";
+            });
+            // DDOES:
+            WSCells[i].addEventListener('mousedown', WSCSelect);
+            // DDOES:
+            WSCells[i].addEventListener('mouseup', WSCSSelect);
+      }
+      document.addEventListener('mouseup', WSCSSelect)
       // DDOES:
       document.getElementById('showSolution').addEventListener('click', function () {
             console.log('showSolution')
       })
 };
-
-
-
+// DFUNC:
+function WSCSelect(e) {
+      console.log(e)
+      // DVARB:
+      mouseUP = false;
+      // DVARN:
+      var MSXC, MSYC, selPat,
+            selPatNum = [],
+            MSXO = e.pageX,
+            MSYO = e.pageY;
+      // DLOOP:
+      for (var k = 0; k < 8; k++) {
+            selPatNum.push([])
+            selPatNum[k].push([], [])
+            for (var j = 0; j < 25; j++) {
+                  if (k = 0) {
+                        selPatNum[k][0].push(function () {
+                              return MSXO + (24 * j);
+                        })
+                  }
+            }
+      }
+      console.log(selPatNum)
+      // DLOOP:
+      for (var k = 0; k < WSCells.length; k++) {
+            // DDOES:
+            WSCells[k].addEventListener('mouseenter', function (e) {
+                  // DDOES:
+                  MSXC = e.pageX, MSYC = e.pageY;
+                  // DIFDO:
+                  if (mouseUP == false) WSCSelecting(e);
+            })
+      }
+      // DDOES:
+      e.preventDefault();
+      e.target.style.backgroundColor = "rgb(255, 200, 185)";
+}
+// DFUNC:
+function WSCSelecting(e) {
+      // 24
+      console.log(e);
+      // DDOES:
+      e.target.style.backgroundColor = "rgb(255, 200, 185)";
+};
+// DFUNC:
+function WSCSSelect(e) {
+      // DVARB:
+      mouseUP = true;
+      // DLOOP:
+      for (var k = 0; k < WSCells.length; k++) {
+            // DDOES:
+            WSCells[k].removeEventListener('mouseenter', function (e) {
+                  console.log(e);
+                  // DDOES:
+                  e.target.style.backgroundColor = "rgb(255, 200, 185)";
+            })
+      }
+}
 /*============================================================*/
 
 function drawWordSearch(letters, words) {
